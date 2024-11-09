96a62fe46f308e0ecfba1d4007ceae2fa9891c02
KRP186zL <krisnabonex5743@gmail.com>
2024-11-09 16:54:42 +0700

    submission-3

--- src/validator/album/AlbumSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const PayloadAlbumSchema = Joi.object({
-  name: Joi.string().required(),
-  year: Joi.number().required(),
-}).unknown(false);
-
-module.exports = { PayloadAlbumSchema };
